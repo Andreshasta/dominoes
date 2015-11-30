3e6846d6bd43cbe8bbdf2482078024507fc27a8e
Edwin <Edwin@EdwinL-PC>
2015-11-30 11:05:42 -0500

    crea PanelOverDraw, falta implementarla EdwinHasta  30-11-2015

--- DominioDomino/src/DominoCliente/ui/PanelOverDraw.java
+++ DominioDomino/src/DominoCliente/ui/PanelOverDraw.java
@@ -1,16 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DominoCliente.ui;
 
+import DominoBase.system.Parametros;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import java.awt.Graphics;
 
-/**
- *
- * @author Edwin
- */
-public class PanelOverDraw extends JPanel{
+
+public class PanelOverDraw extends JPanel {
+
+    private BufferedImage fondo = null;
+    private Graphics2D pinta = null;
+    private int posX;
+    private int posY;
+    private int ancho;
+    private int alto;
     
+    public PanelOverDraw() {
+        posX=1;
+        posY=1;
+        ancho=779;
+        alto=336;
+        File imageFondo = new File(Parametros.PATH_IMAGES + "border.png"); // guarda la imagen en un archivo
+        try {
+            fondo = ImageIO.read(getClass().getResourceAsStream( imageFondo.toString() ) );
+        } catch (IOException ioex) {
+            ioex.printStackTrace();
+        }
+        pinta = fondo.createGraphics();
+        pinta.drawImage(fondo, posX, posY, ancho, alto, this);
+    }
+
+    /*public void pintar(){
+        pinta.drawImage(fondo, posX, posY, ancho, alto, this);
+    }*/
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(fondo, 1, 1, 779, 336, this); // dibuja la imagen al iniciar la aplicacion
+    }
 }
